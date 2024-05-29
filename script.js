103d9660d12e771ac4691c8a10f11aac422fde79
annu666 <148373958+annu666@users.noreply.github.com>
2024-05-29 20:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function fibonacci(num) {
-    let n1 = 0, n2 = 1, nextTerm;
+    let n1 = 0, n2 = 0, nextTerm;
+
+    if (num === 1) {
+        return n1;
+    }
 
     for (let i = 2; i <= num; i++) {
         nextTerm = n1 + n2;
@@ -7,18 +11,13 @@ function fibonacci(num) {
         n2 = nextTerm;
     }
 
-    if(num === 1) {
-        return n1;
-    } else {
-        return n2;
-    }
+    return n2;
 }
 
 function displayFibonacci() {
-    let num = prompt("Enter a number:");
+    let num = parseInt(prompt("Enter a number:"));
     let result = fibonacci(num);
     document.getElementById("result").innerText = "The Fibonacci number is " + result;
 }
 
 module.exports = { fibonacci, displayFibonacci };
-
